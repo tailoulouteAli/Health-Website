de16fe536042c4a5c10ae4fbbb415ad2a1ca4b02
alitailouloute <alitailouloute1990@gmail.com>
2024-10-03 00:31:12 +0200

    ajuout le lien de tailwind css

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-echo "Learning php";
-echo "<br>";
-echo "Ali Tailouloute";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+   <h1 class="text-3xl font-bold text-blue-600">Learning_php</h1>
+</body>
+</html>
