b1a77db35ff68bc29fe274d5d9eeb71f331de2f5
alitailouloute <alitailouloute1990@gmail.com>
2024-10-06 16:48:05 +0200

    logo

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 p-6">
-   <h1 class="text-4xl font-bold text-green-500 mb-4">Learning PHP</h1>
+<body >
+     <h1 class="text-4xl font-bold text-green-500 mb-4">Learning PHP</h1>
+   
    <div class="container mx-auto">
       <?php 
          include "header.php";
