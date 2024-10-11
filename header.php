be84bba82cde0730ec3e34c354ba94938e43228f
alitailouloute <alitailouloute1990@gmail.com>
2024-10-11 19:23:32 +0200

    ajout menu dans header

--- header.php
+++ header.php
@@ -1,3 +1,21 @@
 <?php 
  echo '<h1>header.php</h1>';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+                        <li class="font-mono text-green-400" >Accueil</li>
+                        <li class="font-mono text-green-400">Programme</li>
+                        <li class="font-mono text-green-400">Reference</li>
+                        <li class="font-mono text-green-400">Paramètres</li>
+        </ul>
+    </nav>
+</body>
+</html>
